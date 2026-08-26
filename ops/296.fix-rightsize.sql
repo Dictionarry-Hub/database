929f20b1a1c1fac7b9def05b0c436574fc6d2acb
Seraphys <xshatterx@protonmail.com>
2026-08-26 01:03:43 +0000

    Fix RightSIZE

---
+++ ops/296.fix-rightsize.sql
@@ -0,0 +1,83 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Fix RightSIZE
+-- @exportedAt: 2026-08-26T01:03:41.133Z
+-- @opIds: 13686, 13687, 13688, 13689, 13690, 13691, 13692, 13693, 13694, 13695, 13696, 13697, 13698, 13699, 13700
+
+-- --- BEGIN op 13686 ( update regular_expression "UHD Bluray Release Groups" )
+update "regular_expressions" set "pattern" = '(?<=^|[\s.-])(0BSiDiAN|147|Aio|AJP69|aNDy|ARiN|ASD87|ATELiER|BakedFEL|BAT1|BBW|BMF|BNL|BRUTE|BSTD|BV|CART|c0kE|Chotab|CJ|coffee|CONSORTiUM|CRiME|CRiSC|CRX|CtrlHD|D-Z0N3|Dariush|de[42]|decibeL|DiRTY|DON|E.N.D|E1|E76|EA|EBCP|EbP|Eby|EDPH|ENDSkY|ESiR|EXCiSION|eXterminator|faBR|FANDANGO|FoRM|FraMeSToR|FTO|GALAXY|Geek|GL|GR|GrapeHD|GS88|Gyroscope|GZ|H@M|hdalx|HDL|HDMaNiAcS|HDmonSK|HDV|HDv0T|HDxT|HiDt|HiFi|HiP|HQMUX|HR|IDE|iFT|iLL|iLoveHD|IMNEWHERE|J2G|JewelBox|JKP|JM|KASHMiR|Kitsune|Krispy|Kururun|LAZY|LiNG|LolHD|LoRD|luvBB|MCR|MGs|Narkyy|Natuyuki|NCmt|NiBuRu|NiP|nmd|NorTV|NoVA|NTb|NyHD|O2STK|OB1|OmertaHD|ORBiT|ORiGEN|Otaibi|pcroland|Penumbra|PerfectionHD|PiG30N|PiPicK|playHD|POH|Positive|Prestige|PTer|PuTao|RDK123|REBORN|REPTiLE|RiCO|RightSIZE|RO|Rose3Thorn|rttr|SA89|SaL|SaNcTi|SbR|SiMPLE|Skazhutin|Softboat|SoLaR|SOP|SPHD|SuBHD|TayTo|TBB|TDD|TeamSyndicate|ThD|TnP|ToK|UxO|VD|VietHD|ViSUM|VLAD|VorteX|W4NK3R|WiHD|WiLF|WMING|xander|ZIMBO|ZiNC|ZoroSenpai|ZQ)\b' where "name" = 'UHD Bluray Release Groups' and "pattern" = '(?<=^|[\s.-])(0BSiDiAN|147|Aio|AJP69|aNDy|ARiN|ASD87|ATELiER|BakedFEL|BAT1|BBW|BMF|BNL|BRUTE|BSTD|BV|CART|c0kE|Chotab|CJ|coffee|CONSORTiUM|CRiME|CRiSC|CRX|CtrlHD|D-Z0N3|Dariush|de[42]|decibeL|DiRTY|DON|E.N.D|E1|E76|EA|EBCP|EbP|Eby|EDPH|ENDSkY|ESiR|EXCiSION|eXterminator|faBR|FANDANGO|FoRM|FraMeSToR|FTO|GALAXY|Geek|GL|GR|GrapeHD|GS88|Gyroscope|GZ|H@M|hdalx|HDL|HDMaNiAcS|HDmonSK|HDV|HDv0T|HDxT|HiDt|HiFi|HiP|HQMUX|HR|IDE|iFT|iLL|iLoveHD|IMNEWHERE|J2G|JewelBox|JKP|JM|KASHMiR|Kitsune|Krispy|Kururun|LAZY|LiNG|LolHD|LoRD|luvBB|MCR|MGs|Narkyy|Natuyuki|NCmt|NiBuRu|NiP|nmd|NorTV|NoVA|NTb|NyHD|O2STK|OB1|OmertaHD|ORBiT|ORiGEN|Otaibi|pcroland|Penumbra|PerfectionHD|PiG30N|PiPicK|playHD|POH|Positive|Prestige|PTer|PuTao|RDK123|REBORN|REPTiLE|RiCO|rightSIZE|RO|Rose3Thorn|rttr|SA89|SaL|SaNcTi|SbR|SiMPLE|Skazhutin|Softboat|SoLaR|SOP|SPHD|SuBHD|TayTo|TBB|TDD|TeamSyndicate|ThD|TnP|ToK|UxO|VD|VietHD|ViSUM|VLAD|VorteX|W4NK3R|WiHD|WiLF|WMING|xander|ZIMBO|ZiNC|ZoroSenpai|ZQ)\b';
+-- --- END op 13686
+
+-- --- BEGIN op 13687 ( update regular_expression "UHD WEBRip Release Groups" )
+update "regular_expressions" set "pattern" = '(?<=^|[\s.-])(0BSiDiAN|147|Aio|AJP69|aNDy|ARiN|ASD87|ATELiER|BakedFEL|BAT1|BBW|BMF|BNL|BRUTE|BSTD|BV|CART|c0kE|Chotab|CJ|coffee|CONSORTiUM|CRiME|CRiSC|CRX|CtrlHD|D-Z0N3|Dariush|de[42]|decibeL|DiRTY|DON|E.N.D|E1|E76|EA|EBCP|EbP|Eby|EDPH|ENDSkY|ESiR|EXCiSION|eXterminator|faBR|FANDANGO|FoRM|FraMeSToR|FTO|GALAXY|Geek|GL|GR|GrapeHD|GS88|Gyroscope|GZ|H@M|hdalx|HDL|HDMaNiAcS|HDmonSK|HDV|HDv0T|HDxT|HiDt|HiFi|HiP|HQMUX|HR|IDE|iFT|iLL|iLoveHD|IMNEWHERE|J2G|JewelBox|JKP|JM|KASHMiR|Kitsune|Krispy|Kururun|LAZY|LiNG|LolHD|LoRD|luvBB|MCR|MGs|Narkyy|Natuyuki|NCmt|NiBuRu|NiP|nmd|NorTV|NoVA|NyHD|O2STK|OB1|OmertaHD|ORBiT|ORiGEN|Otaibi|pcroland|Penumbra|PerfectionHD|PiG30N|PiPicK|playHD|POH|Positive|Prestige|PTer|PuTao|RDK123|REBORN|REPTiLE|RiCO|RightSIZE|RO|Rose3Thorn|rttr|SA89|SaL|SaNcTi|SbR|SiMPLE|Skazhutin|Softboat|SoLaR|SOP|SPHD|SuBHD|TayTo|TBB|TDD|TeamSyndicate|ThD|TnP|ToK|UxO|VD|VietHD|ViSUM|VLAD|VorteX|W4NK3R|WiHD|WiLF|WMING|xander|ZIMBO|ZiNC|ZoroSenpai|ZQ)\b' where "name" = 'UHD WEBRip Release Groups' and "pattern" = '(?<=^|[\s.-])(0BSiDiAN|147|Aio|AJP69|aNDy|ARiN|ASD87|ATELiER|BakedFEL|BAT1|BBW|BMF|BNL|BRUTE|BSTD|BV|CART|c0kE|Chotab|CJ|coffee|CONSORTiUM|CRiME|CRiSC|CRX|CtrlHD|D-Z0N3|Dariush|de[42]|decibeL|DiRTY|DON|E.N.D|E1|E76|EA|EBCP|EbP|Eby|EDPH|ENDSkY|ESiR|EXCiSION|eXterminator|faBR|FANDANGO|FoRM|FraMeSToR|FTO|GALAXY|Geek|GL|GR|GrapeHD|GS88|Gyroscope|GZ|H@M|hdalx|HDL|HDMaNiAcS|HDmonSK|HDV|HDv0T|HDxT|HiDt|HiFi|HiP|HQMUX|HR|IDE|iFT|iLL|iLoveHD|IMNEWHERE|J2G|JewelBox|JKP|JM|KASHMiR|Kitsune|Krispy|Kururun|LAZY|LiNG|LolHD|LoRD|luvBB|MCR|MGs|Narkyy|Natuyuki|NCmt|NiBuRu|NiP|nmd|NorTV|NoVA|NyHD|O2STK|OB1|OmertaHD|ORBiT|ORiGEN|Otaibi|pcroland|Penumbra|PerfectionHD|PiG30N|PiPicK|playHD|POH|Positive|Prestige|PTer|PuTao|RDK123|REBORN|REPTiLE|RiCO|rightSIZE|RO|Rose3Thorn|rttr|SA89|SaL|SaNcTi|SbR|SiMPLE|Skazhutin|Softboat|SoLaR|SOP|SPHD|SuBHD|TayTo|TBB|TDD|TeamSyndicate|ThD|TnP|ToK|UxO|VD|VietHD|ViSUM|VLAD|VorteX|W4NK3R|WiHD|WiLF|WMING|xander|ZIMBO|ZiNC|ZoroSenpai|ZQ)\b';
+-- --- END op 13687
+
+-- --- BEGIN op 13688 ( update regular_expression "rightSIZE" )
+update "regular_expressions" set "pattern" = '(?<=^|[\s.-])RightSIZE\b' where "name" = 'rightSIZE' and "pattern" = '(?<=^|[\s.-])rightSIZE\b';
+-- --- END op 13688
+
+-- --- BEGIN op 13689 ( update regular_expression "idiot" )
+update "regular_expressions" set "name" = 'idiot' where "name" = 'rightSIZE';
+-- --- END op 13689
+
+-- --- BEGIN op 13690 ( update custom_format "1080p Quality Tier 5" )
+update "condition_patterns" set "regular_expression_name" = 'idiot' where "custom_format_name" = '1080p Quality Tier 5' and "condition_name" = 'rightSIZE' and "regular_expression_name" in ('rightSIZE', 'idiot');
+-- --- END op 13690
+
+-- --- BEGIN op 13691 ( update custom_format "1080p Quality Tier 5 (Efficient)" )
+update "condition_patterns" set "regular_expression_name" = 'idiot' where "custom_format_name" = '1080p Quality Tier 5 (Efficient)' and "condition_name" = 'rightSIZE' and "regular_expression_name" in ('rightSIZE', 'idiot');
+-- --- END op 13691
+
+-- --- BEGIN op 13692 ( update custom_format "720p Quality Tier 3" )
+update "condition_patterns" set "regular_expression_name" = 'idiot' where "custom_format_name" = '720p Quality Tier 3' and "condition_name" = 'rightSIZE' and "regular_expression_name" in ('rightSIZE', 'idiot');
+-- --- END op 13692
+
+-- --- BEGIN op 13693 ( update regular_expression "RightSIZE" )
+update "regular_expressions" set "name" = 'RightSIZE' where "name" = 'idiot';
+-- --- END op 13693
+
+-- --- BEGIN op 13694 ( update custom_format "1080p Quality Tier 5" )
+update "condition_patterns" set "regular_expression_name" = 'RightSIZE' where "custom_format_name" = '1080p Quality Tier 5' and "condition_name" = 'rightSIZE' and "regular_expression_name" in ('idiot', 'RightSIZE');
+-- --- END op 13694
+
+-- --- BEGIN op 13695 ( update custom_format "1080p Quality Tier 5 (Efficient)" )
+update "condition_patterns" set "regular_expression_name" = 'RightSIZE' where "custom_format_name" = '1080p Quality Tier 5 (Efficient)' and "condition_name" = 'rightSIZE' and "regular_expression_name" in ('idiot', 'RightSIZE');
+-- --- END op 13695
+
+-- --- BEGIN op 13696 ( update custom_format "720p Quality Tier 3" )
+update "condition_patterns" set "regular_expression_name" = 'RightSIZE' where "custom_format_name" = '720p Quality Tier 3' and "condition_name" = 'rightSIZE' and "regular_expression_name" in ('idiot', 'RightSIZE');
+-- --- END op 13696
+
+-- --- BEGIN op 13697 ( update custom_format "1080p Quality Tier 5" )
+DELETE FROM custom_format_conditions
+	WHERE custom_format_name = '1080p Quality Tier 5'
+	  AND name = 'rightSIZE'
+	  AND type = 'release_group'
+	  AND arr_type = 'all'
+	  AND negate = 0
+	  AND required = 0;
+-- --- END op 13697
+
+-- --- BEGIN op 13698 ( update custom_format "1080p Quality Tier 5" )
+INSERT INTO custom_format_conditions (custom_format_name, name, type, arr_type, negate, required)
+VALUES ('1080p Quality Tier 5', 'RightSIZE', 'release_group', 'all', 0, 0);
+
+INSERT INTO condition_patterns (custom_format_name, condition_name, regular_expression_name) VALUES ('1080p Quality Tier 5', 'RightSIZE', 'RightSIZE');
+-- --- END op 13698
+
+-- --- BEGIN op 13699 ( update custom_format "1080p Quality Tier 5 (Efficient)" )
+DELETE FROM custom_format_conditions
+	WHERE custom_format_name = '1080p Quality Tier 5 (Efficient)'
+	  AND name = 'rightSIZE'
+	  AND type = 'release_group'
+	  AND arr_type = 'all'
+	  AND negate = 0
+	  AND required = 0;
+-- --- END op 13699
+
+-- --- BEGIN op 13700 ( update custom_format "1080p Quality Tier 5 (Efficient)" )
+INSERT INTO custom_format_conditions (custom_format_name, name, type, arr_type, negate, required)
+VALUES ('1080p Quality Tier 5 (Efficient)', 'RightSIZE', 'release_group', 'all', 0, 0);
+
+INSERT INTO condition_patterns (custom_format_name, condition_name, regular_expression_name) VALUES ('1080p Quality Tier 5 (Efficient)', 'RightSIZE', 'RightSIZE');
+-- --- END op 13700
